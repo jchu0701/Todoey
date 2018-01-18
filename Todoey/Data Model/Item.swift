88b21ec44e520ccae65785c51c86055e443e9237
Greg Chu <gregchuosu@gmail.com>
2018-01-17 19:54:23 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by greg chu on 1/16/18.
+//  Copyright © 2018 Greg Chu. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
